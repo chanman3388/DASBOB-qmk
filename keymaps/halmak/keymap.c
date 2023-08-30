d1b1dc93ceb99a0d715fdf0b0bc2c1af19545c65
chanman3388 <cwtchan3388@gmail.com>
2023-08-30 02:18:33 +0100

    fix syntax error

--- keymaps/halmak/keymap.c
+++ keymaps/halmak/keymap.c
@@ -37,7 +37,7 @@ tap_dance_action_t tap_dance_actions[] = {
 
 td_state_t cur_dance(tap_dance_state_t *state) {
     if (state->count == 1) {
-        else return TD_SINGLE_HOLD;
+        return TD_SINGLE_HOLD;
     } else if (state->count == 2) {
         if (state->pressed) return TD_DOUBLE_HOLD;
         else return TD_DOUBLE_TAP;
