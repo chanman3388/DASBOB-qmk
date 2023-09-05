7a5b65a9294841f48b1a1877b9afe26008665129
Chris Chan <chris.chan@techex.co.uk>
2023-09-05 08:48:08 +0100

    more keymap changes

--- keymaps/halmak/keymap.c
+++ keymaps/halmak/keymap.c
@@ -4,7 +4,8 @@ enum dasbob_layers {
     _HALMAK,
     _LOWER,
     _RAISE,
-    _SUPER
+    _SUPER,
+    _FUNCTION
 };
 
 typedef enum {
@@ -126,24 +127,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_HALMAK] = LAYOUT_split_3x5_3(
-        KC_W,          KC_L,          KC_R,            KC_B,             KC_Z,               KC_SCLN,         KC_Q,         KC_U,         KC_D,           KC_J,
-        KC_S,          KC_H,          KC_N,            KC_T,             KC_COMM,            KC_DOT,          KC_A,         KC_E,         KC_O,           KC_I,
-        KC_F,          KC_M,          KC_V,            KC_C,             KC_SLSH,            KC_G,            KC_P,         KC_X,         KC_K,           KC_Y,
-                                      TD(TD_ALT_WIN),  TD(L1_SHFT),      LCTL_T(KC_SPC),     LSFT_T(KC_ENT),  TD(L2_CTRL),  KC_BSPC
+        KC_W,                   KC_L,               KC_R,           KC_B,               KC_Z,               KC_SCLN,            KC_Q,           KC_U,           KC_D,           KC_J,
+        KC_S,                   KC_H,               KC_N,           KC_T,               KC_COMM,            KC_DOT,             KC_A,           KC_E,           KC_O,           KC_I,
+        LT(_FUNCTION, KC_F),    KC_M,               KC_V,           KC_C,               KC_SLSH,            KC_G,               KC_P,           KC_X,           KC_K,           KC_Y,
+                                TD(TD_ALT_WIN),     TD(L1_SHFT),    LCTL_T(KC_SPC),     LSFT_T(KC_ENT),     TD(L2_CTRL),        KC_BSPC
     ),
 
     [_LOWER] = LAYOUT_split_3x5_3(
-        KC_1,          KC_2,          KC_3,            KC_4,             KC_5,               KC_F1,           KC_F2,        KC_F3,            KC_F4,              KC_DEL,
-        KC_6,          KC_7,          KC_8,            KC_9,             KC_0,               KC_F5,           KC_F6,        KC_F7,            KC_F8,              XXXXXXX,
-        RALT(KC_4),    KC_NUBS,       KC_MINS,         KC_EQL,           KC_NUHS,            KC_F9,           KC_F10,       KC_F11,           KC_F12,             XXXXXXX,
-                                      KC_TRNS,         KC_TRNS,          KC_TRNS,            KC_TRNS,         KC_TRNS,      KC_TAB
+        KC_1,                   KC_2,               KC_3,            KC_4,              KC_5,               S(KC_1),            S(KC_2),        S(KC_3),        S(KC_4),        S(KC_5),
+        KC_6,                   KC_7,               KC_8,            KC_9,              KC_0,               S(KC_6),            S(KC_7),        S(KC_8),        KC_QUOT,        KC_NUHS,
+        KC_GRV,                 KC_NUBS,            KC_MINS,         KC_EQL,            KC_NUHS,            S(KC_NUHS),         S(KC_EQL),      S(KC_MINS),     S(KC_NUBS),     KC_DEL,
+                                                    KC_TRNS,         KC_TRNS,           KC_TRNS,            KC_TRNS,            KC_TRNS,        KC_TAB
     ),
 
     [_RAISE] = LAYOUT_split_3x5_3(
-        XXXXXXX,       XXXXXXX,       KC_LBRC,         KC_RBRC,          XXXXXXX,            KC_QUOT,         KC_UP,        S(KC_QUOT),     KC_HOME,     KC_PGUP,
-        KC_NUBS,       S(KC_NUBS),    S(KC_9),         S(KC_0),          XXXXXXX,            KC_LEFT,         KC_DOWN,      KC_RIGHT,       KC_END,      KC_PGDN,
-        S(KC_NUHS),    KC_NUHS,       S(KC_LBRC),      S(KC_RBRC),       XXXXXXX,            XXXXXXX,         XXXXXXX,      XXXXXXX,        XXXXXXX,     XXXXXXX,
-                                      KC_TRNS,         KC_TRNS,          KC_TRNS,            KC_TRNS,         KC_TRNS,      KC_ESC
+        XXXXXXX,                XXXXXXX,            KC_LBRC,         KC_RBRC,           XXXXXXX,            KC_QUOT,            KC_UP,          S(KC_QUOT),     KC_HOME,     KC_PGUP,
+        XXXXXXX,                XXXXXXX,            S(KC_9),         S(KC_0),           RALT(KC_4),         KC_LEFT,            KC_DOWN,        KC_RIGHT,       KC_END,      KC_PGDN,
+        S(KC_NUHS),             KC_NUHS,            S(KC_LBRC),      S(KC_RBRC),        XXXXXXX,            XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,     XXXXXXX,
+                                                    KC_TRNS,         KC_TRNS,           KC_TRNS,            KC_TRNS,            KC_TRNS,        KC_ESC
     ),
 
     [_SUPER] = LAYOUT_split_3x5_3(
@@ -151,6 +152,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,       XXXXXXX,       A(S(KC_TAB)),    A(KC_TAB),        XXXXXXX,            XXXXXXX,         C(G(KC_LEFT)),  C(G(KC_RIGHT)),   XXXXXXX, XXXXXXX,
         XXXXXXX,       XXXXXXX,       C(S(KC_TAB)),    C(KC_TAB),        XXXXXXX,            XXXXXXX,         C(S(KC_TAB)),   C(KC_TAB),        XXXXXXX, XXXXXXX,
                                       KC_TRNS,         KC_TRNS,          KC_TRNS,            KC_TRNS,         KC_TRNS,        KC_TRNS
+    ),
+
+    [_FUNCTION] = LAYOUT_split_3x5_3(
+        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          XXXXXXX,
+        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          XXXXXXX,
+        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_F9,          KC_F10,         KC_F11,         KC_F12,         XXXXXXX,
+                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS
     ),
 
 };
@@ -291,25 +299,29 @@ bool oled_task_user(void) {
 
     switch (get_highest_layer(layer_state)) {
         case _HALMAK:
-        oled_set_cursor(12, 1);
+            oled_set_cursor(12, 1);
             oled_write_P(PSTR("halmak\n"), false);
             break;
         case _LOWER:
-        oled_set_cursor(12, 1);
+            oled_set_cursor(12, 1);
             oled_write_P(PSTR("lower\n"), false);
             break;
         case _RAISE:
-        oled_set_cursor(12, 1);
+            oled_set_cursor(12, 1);
             oled_write_P(PSTR("raise\n"), false);
             break;
         case _SUPER:
-        oled_set_cursor(12, 1);
+            oled_set_cursor(12, 1);
             oled_write_P(PSTR("super\n"), false);
+            break;
+        case _FUNCTION:
+            oled_set_cursor(12, 1);
+            oled_write_P(PSTR("function\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
-        oled_set_cursor(6, 1);
-        oled_write_ln_P(PSTR("Undefined"), false);
+            oled_set_cursor(6, 1);
+            oled_write_ln_P(PSTR("Undefined"), false);
     }
     } else {
         static const char PROGMEM dasbob_logo[] = {
