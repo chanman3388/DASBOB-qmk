c6ee03ad9dd25ebe89eba954528fa15b08d39e8d
chanman3388 <cwtchan3388@gmail.com>
2023-11-22 02:08:17 +0000

    remove some redundant key assignments, add tab, esc, and backspace to _RAISE layer

--- keymaps/halmak/keymap.c
+++ keymaps/halmak/keymap.c
@@ -141,9 +141,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RAISE] = LAYOUT_split_3x5_3(
-        XXXXXXX,                XXXXXXX,            KC_LBRC,         KC_RBRC,           XXXXXXX,            KC_QUOT,            KC_UP,          S(KC_QUOT),     KC_HOME,     KC_PGUP,
+        KC_TAB,                 XXXXXXX,            KC_LBRC,         KC_RBRC,           XXXXXXX,            KC_QUOT,            KC_UP,          S(KC_QUOT),     KC_HOME,     KC_PGUP,
         XXXXXXX,                XXXXXXX,            S(KC_9),         S(KC_0),           RALT(KC_4),         KC_LEFT,            KC_DOWN,        KC_RIGHT,       KC_END,      KC_PGDN,
-        S(KC_NUHS),             KC_NUHS,            S(KC_LBRC),      S(KC_RBRC),        XXXXXXX,            XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,     XXXXXXX,
+        KC_TAB,                 XXXXXXX,            S(KC_LBRC),      S(KC_RBRC),        XXXXXXX,            XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,     KC_BSPC,
                                                     KC_TRNS,         KC_TRNS,           KC_TRNS,            KC_TRNS,            KC_TRNS,        KC_ESC
     ),
 
